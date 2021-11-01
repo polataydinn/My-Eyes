e01fd07f020bf4e6eec0699be1074bfa336ca387
Monster <htas767@gmail.com>
2021-11-01 23:18:40 +0300

    bottom navigation added

--- app/src/main/java/com/example/myeyes/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/myeyes/presentation/ui/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.myeyes.presentation.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myeyes.R
+import com.example.myeyes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_MyEyes)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.background = null
     }
 }
